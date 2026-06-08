0332807c71f061f16234287ce68d62c8506df3ae
singhritik62 <singhritik62@gmail.com>
2026-06-08 15:42:15 +0530

    chore: remove entire backend directory

--- backend/src/tests/api/pools.test.ts
+++
@@ -1,9 +0,0 @@
-import request from "supertest";
-import app from "../../index.js";
-
-describe("Pools API", () => {
-  it("returns pools", async () => {
-    const response = await request(app).get("/api/pools");
-    expect(response.status).toBe(200);
-  });
-});
--- backend/src/tests/performance/benchmark.ts
+++
@@ -1,5 +0,0 @@
-console.time("quote");
-for (let i = 0; i < 10000; i++) {
-  Math.sqrt(i);
-}
-console.timeEnd("quote");
